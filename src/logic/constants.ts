4e28a7ea56e131bf606cc2a9db5009231d153834
JW <jiangweixian1994@gmail.com>
2022-04-16 16:50:37 +0800

    feat: monorepo (#4)

--- src/logic/constants.ts
+++ src/logic/constants.ts
@@ -5,4 +5,5 @@ export const TOGGLE_MODAL = 'TOGGLE_MODAL'
 export const REQUEST_NPM_DETAIL = 'REQUEST_NPM_DETAIL'
 
 export const GITHUB_RAW_PREFIX = 'https://raw.githubusercontent.com/'
+export const GITHUB_REST_PREFIX = 'https://api.github.com/'
 export const NPM_REGISTRY_PREFIX = 'https://registry.npmjs.org/'
